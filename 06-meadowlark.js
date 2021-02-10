cbba94e9ba9809d540768267725e50514bf6277b
Gadalean Gabriel <gadaleang@gmail.com>
2021-02-10 15:48:34 +0200

    Signed-off-by: Gadalean Gabriel <gadaleang@gmail.com>

--- 06-meadowlark.js
+++ 06-meadowlark.js
@@ -43,6 +43,15 @@ app.get('/text', (req, res) => {
     res.send('Acesta este un test')
 })
 
+app.put('/api/tour/:id', (req, res) => {
+    const p = tours.find(p => p.id === parseInt(req.params.id))
+    if(!p) return res.status(404).json({ error: 'No such tour exists' })
+    if(req.body.name) p.name = req.body.name
+    if(req.body.price) p.price = req.body.price
+    res.json({ success: true })
+    })
+
+
 app.get('/api/tours', (req, res) => {
     const toursXml = '<?xml version="1.0"?><tours>' + 
     tours.map(p =>
