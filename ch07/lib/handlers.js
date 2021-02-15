432c942136f8f41d62469be96cad4522d592f84a
Gadalean Gabriel <gadaleang@gmail.com>
2021-02-15 16:05:55 +0200

    Signed-off-by: Gadalean Gabriel <gadaleang@gmail.com>

--- ch07/lib/handlers.js
+++ ch07/lib/handlers.js
@@ -4,7 +4,7 @@ exports.sectionTest = (req, res) => res.render('section-test')
 exports.notFound = (req, res) => res.render('404')
 
 // Express recognizes the error handler by way of its four
-// argumetns, so we have to disable ESLint's no-unused-vars rule
+// arguments, so we have to disable ESLint's no-unused-vars rule
 /* eslint-disable no-unused-vars */
 exports.serverError = (err, req, res, next) => res.render('500')
 /* eslint-enable no-unused-vars */
